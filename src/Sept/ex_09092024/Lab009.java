5e11df0fc55a07ebeb42f16ea4ce765a5ea0b8ef
Parag Gharat <gharatpj@gmail.com>
2024-09-13 02:36:03 +0530

    Commit file push

---
+++ src/Sept/ex_09092024/Lab009.java
@@ -0,0 +1,12 @@
+package Sept.ex_09092024;
+
+public class Lab009 {
+    public static void main(String[] args) {
+        //Keywords
+        //Reserved Words
+        //Simple English -> Pre Define Meaning in Java Programs.
+        //They are Blue in Intellij
+        //package, public, class, static, void
+        //Lower case
+    }
+}
---
+++ src/Sept/ex_09092024/Lab010.java
@@ -0,0 +1,10 @@
+package Sept.ex_09092024;
+
+public class Lab010 {
+    public static void main(String[] args) {
+        //Variables and Data Types, Identifiers
+        boolean is_mohit_married = true;
+        boolean do_you_have_audi = false;
+        boolean do_you_have_bmw = false;
+    }
+}
