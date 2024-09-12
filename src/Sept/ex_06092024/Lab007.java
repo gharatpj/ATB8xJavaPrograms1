e156107d7aa6b0025028e7f8dfcfe8ba2faf8499
Parag Gharat <gharatpj@gmail.com>
2024-09-12 17:39:49 +0530

    Commit file push

---
+++ src/Sept/ex_06092024/Lab007.java
@@ -0,0 +1,12 @@
+package Sept.ex_06092024;
+
+public class Lab007 {
+    public static void main(String[] args) {
+        System.out.println("Hi Sir");
+        //this code will not executed
+        //this is used to show the comments
+
+        System.out.println("Hi Sir");
+
+    }
+}
