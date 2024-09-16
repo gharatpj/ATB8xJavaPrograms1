28ae9d2fbe92c9bb8768c2e17d5fbcd2692ff043
Parag Gharat <gharatpj@gmail.com>
2024-09-17 02:26:01 +0530

    Commit file push

--- src/Sept/ex_13092024/Lab019_Task1.java
+++ src/Sept/ex_13092024/Lab019_Task1.java
@@ -2,6 +2,13 @@ package Sept.ex_13092024;
 
 public class Lab019_Task1 {
     public static void main(String[] args) {
+        //Write a program to print the table of number which user will give( int num 10)
+        //output will be like this (by using the printf()
+        //10 × 1 = 10
+        //10 × 2 = 20
+        //10 × 3 = 30….
+        //10 × 10 = 100.
+
         int num = 10;
         System.out.printf("%d * %d = %d",num,1,num*1);
         System.out.println();
--- src/Sept/ex_13092024/Lab020_Task2.java
+++ src/Sept/ex_13092024/Lab020_Task2.java
@@ -2,6 +2,11 @@ package Sept.ex_13092024;
 
 public class Lab020_Task2 {
     public static void main(String[] args) {
+        //Prepare a small calculator, which supports +, - , , / and %(?) - Modulus operator
+        //double a = 34;
+        //double b = 10;
+        //Print - a+b, a-b, ab, a/b, a%b all the outputs.
+
         double a = 34;
         double b = 10;
         System.out.println(a+b);
