2e5948b8de6a0bc91c6b13013f30472c7e140381
Parag Gharat <gharatpj@gmail.com>
2024-09-16 18:18:52 +0530

    Commit file push

---
+++ src/Sept/ex_11092024/Lab011.java
@@ -0,0 +1,15 @@
+package Sept.ex_11092024;
+
+public class Lab011 {
+    public static void main(String[] args) {
+        //variable_name (Identifier) - Rules we have to follow
+        //boolean 123 = true; This is wrong
+
+        boolean $ = true;
+        boolean _ = true;
+        boolean parag_is_handsome = true;
+        boolean PARAG_GHARAT = true;
+        boolean _123 = true;
+        System.out.println(_123);
+    }
+}
---
+++ src/Sept/ex_11092024/Lab013_Data_Type.java
@@ -0,0 +1,13 @@
+package Sept.ex_11092024;
+
+public class Lab013_Data_Type {
+    public static void main(String[] args) {
+        //Store the distance between earth and moon
+        long distance_between_earth_moon = 384400000l; //in M
+        double distance_between_earth_moon2 = 384400000.897; //in M
+
+        //max age
+        byte age = 122;
+        int age1 = 100; // wasting memory = 2353463463465
+    }
+}
