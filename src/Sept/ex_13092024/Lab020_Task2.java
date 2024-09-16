ab7307967a6455b092bc176169dba68aa1918ea7
Parag Gharat <gharatpj@gmail.com>
2024-09-17 02:05:47 +0530

    Commit file push

--- src/Sept/ex_13092024/Lab020_Task2.java
+++ src/Sept/ex_13092024/Lab020_Task2.java
@@ -9,14 +9,5 @@ public class Lab020_Task2 {
         System.out.println(a*b);
         System.out.println(a/b);
         System.out.println(a%b);
-        System.out.printf( "Addition of two numbers is %.1f + %.1f = %.1f",a,b, a+b);
-        System.out.println();
-        System.out.printf( "Subtraction of two numbers is %.1f - %.1f = %.1f",a,b, a-b);
-        System.out.println();
-        System.out.printf( "Multiplication of two numbers is %.1f * %.1f = %.1f",a,b, a*b);
-        System.out.println();
-        System.out.printf( "Division of two numbers is %.1f / %.1f = %.1f",a,b, a/b);
-        System.out.println();
-        System.out.printf( "Modulus of two numbers is %.1f %.1f = %.1f",a,b, a%b);
     }
 }
