a545f22d6583820aac12120010fea04a8188ff18
Parag Gharat <gharatpj@gmail.com>
2024-09-27 01:54:09 +0530

    Commit file push

---
+++ src/Sept/ex_25092024/Lab081_Task1.java
@@ -0,0 +1,18 @@
+package Sept.ex_25092024;
+
+public class Lab081_Task1 {
+    public static void main(String[] args) {
+        //write code to print table n=10 with For or while loop
+        int num =10;
+        for(int i=1; i<=10; i++)
+        {
+            System.out.println(num*i);
+        }
+
+//        int i =1;
+//        while(i<=10){
+//        System.out.println(num*i);
+//            i++;
+
+        }
+    }
