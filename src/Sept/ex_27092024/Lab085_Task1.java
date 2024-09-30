6e9c843e298ec756024e562e020f53707d0b3e0c
Parag Gharat <gharatpj@gmail.com>
2024-09-30 16:38:34 +0530

    Commit file push

---
+++ src/Sept/ex_27092024/Lab085_Task1.java
@@ -0,0 +1,16 @@
+package Sept.ex_27092024;
+
+public class Lab085_Task1 {
+    public static void main(String[] args) {
+        //Create a Java program that prints the first 5 odd numbers using a do-while loop.
+
+        int b = 1;
+        System.out.println("Printing first five Even numbers:");
+        do{
+            System.out.println(b*2);
+            b++;
+        }while(b<=5);
+    }
+
+
+}
---
+++ src/Sept/ex_27092024/Lab086_Task3.java
@@ -0,0 +1,15 @@
+package Sept.ex_27092024;
+
+public class Lab086_Task3 {
+    public static void main(String[] args) {
+      //Write a Java program that calculates the sum of numbers
+        // from 1 to 100 using a while loop.
+        int sum =0;
+        int i = 1;
+        while (i <100){
+            sum = sum + i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
